7988540dbde39f05825761ee251f2ae6dddf7eb3
qolbudr <qolbu.dzikru30@gmail.com>
2021-12-18 13:25:07 +0700

    relayout ipad

--- route/page/script.js
+++ route/page/script.js
@@ -5,3 +5,18 @@ document.querySelector('iframe').src = 'https://youtube.com/embed/' + youtubeId;
 
 
 document.querySelector('#joinzoom').href = zoomLink;
+
+function toggleFullScreen() {
+   var doc = window.document;
+   var docEl = doc.documentElement;
+
+   var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+   var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+   if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+       requestFullScreen.call(docEl);
+   }
+   else {
+       cancelFullScreen.call(doc);
+   }
+}
